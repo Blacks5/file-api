aeb4d811f150ac486b68d69d82e404783216ea6a
一步之瑶 <=wuya520123@vip.qq.com>
2017-10-23 16:53:46 +0800

    引用"ramsey/uuid": "~3.6.1" 处理uuid

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 2017/10/23
+ * Time: 14:09
+ */
+
+namespace App\Http\Controllers;
+
+
+class FileController
+{
+
+}
---
+++ app/Services/OSS.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 2017/10/23
+ * Time: 13:34
+ */
+
+namespace App\Services;
+
+
+class OSS
+{
+
+}
