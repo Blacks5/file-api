8713a5822819ee3ef8580cb121fd4aa102e714f0
一步之瑶 <=wuya520123@vip.qq.com>
2017-10-24 10:44:32 +0800

    增加文件获取类获取图片地址

--- app/Services/FileGet.php
+++ app/Services/FileGet.php
@@ -9,7 +9,27 @@
 namespace App\Services;
 
 
+use App\File;
+use Carbon\Carbon;
+use Faker\Provider\cs_CZ\DateTime;
+
 class FileGet
 {
+    public function getFile($uuid)
+    {
+        $file = File::where('uuid', $uuid)->firstOrFail();
 
+        return $this->aliyunGetPrivateFile($file->path.$file->uuid);
+    }
+
+    private function aliyunGetPublicFile($path)
+    {
+        return OSS::getPublicObjectURL(env('OSS_TEST_BUCKET'), $path);
+
+    }
+
+    private function aliyunGetPrivateFile($path)
+    {
+        return OSS::getPrivateObjectURLWithExpireTime(env('OSS_TEST_BUCKET'), $path, Carbon::now()->addDay(1));
+    }
 }
