6af7cd3bb0214090efc27be472f5c4ab42a904b1
OneStep <wuya520123@vip.qq.com>
2017-10-25 19:39:34 +0800

    阿里云oss获取图片

--- app/Repository/Get/AliyunGet.php
+++ app/Repository/Get/AliyunGet.php
@@ -9,7 +9,22 @@
 namespace App\Repository\Get;
 
 
-class AliyunGet
-{
+use App\Services\OSS;
+use Carbon\Carbon;
 
+class AliyunGet implements BaseGet
+{
+    private $bucket;
+    private $expireTime;
+
+    public function __construct()
+    {
+        $this->bucket = env('OSS_TEST_BUCKET');
+        $this->expireTime = Carbon::now()->addDay(1);
+    }
+
+    public function getImg($path)
+    {
+        return OSS::getPrivateObjectURLWithExpireTime($this->bucket, $path, $this->expireTime);
+    }
 }
